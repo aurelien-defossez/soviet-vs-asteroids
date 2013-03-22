6c244cbecdf8ca0fb4b61b013430f07a268f6a93
Maxime Viry <chinow42@gmail.com>
2013-03-23 00:38:16 +0100

    adding joystick support drawing lines

--- game/src/Game.lua
+++ game/src/Game.lua
@@ -5,10 +5,12 @@
 -- The main game class.
 --
 -----------------------------------------------------------------------------------------
-
 module("Game", package.seeall)
 local Class = Game
 Class.__index = Class
+
+local joystick1Position = 0;
+local joystick2Position = 0;
 
 -----------------------------------------------------------------------------------------
 -- Imports
@@ -17,6 +19,7 @@ Class.__index = Class
 require("lib.math.vec2")
 require("lib.math.aabb")
 require("src.Config")
+require("src.Station")
 
 -----------------------------------------------------------------------------------------
 -- Initialization and Destruction
@@ -24,26 +27,28 @@ require("src.Config")
 
 -- Create the game
 function Class.create(options)
-	-- Create object
-	self = {}
-	setmetatable(self, Class)
+    -- Create object
+    self = {}
+    setmetatable(self, Class)
 
     -- Set virtual viewport
-	self.virtualScreenHeight = gameConfig.camera.minVirtualHeight
-	self.virtualScaleFactor = love.graphics.getHeight() / self.virtualScreenHeight
-	self.screenRatio = love.graphics.getWidth() / love.graphics.getHeight()
-	self.camera = vec2(0, 0)
-	self.zoom = 1.0
+    self.virtualScreenHeight = gameConfig.camera.minVirtualHeight
+    self.virtualScaleFactor = love.graphics.getHeight() / self.virtualScreenHeight
+    self.screenRatio = love.graphics.getWidth() / love.graphics.getHeight()
+    self.camera = vec2(0, 0)
+    self.zoom = 1.0
 
-	-- Set font
-	love.graphics.setFont(love.graphics.newFont(40))
+    -- Set font
+    love.graphics.setFont(love.graphics.newFont(40))
 
-	-- Create debug shape
-	self.x = 0
-	self.y = 0
-	self.radius = 32
+    -- Create debug shape
+    self.x = 0
+    self.y = 0
+    self.radius = 32
 
-	return self
+    self.station = Station.create();
+
+    return self
 end
 
 -- Destroy the game
@@ -59,40 +64,27 @@ end
 -- Parameters:
 --  dt: The time in seconds since last frame
 function Class:update(dt)
-	self.x = self.x + dt * 10000
-	self.y = self.y + dt * 10000
+    self.station:update(dt)
+
+    --love.joystick.open(1)
+    local axisDir1, axisDir2, axisDir3, axisDir4, axisDir5  = love.joystick.getAxes( 1 )
+    -- Joystick
+    -- 1 X et 2 Y, left
+    -- 3  L2, R2
+    -- 4 Y et 5 X
+    self.x = self.x + dt * 10000 * axisDir2 * 100
+   -- self.y = self.y + dt * 10000 * axisDir7 * 100
+    love.graphics.setColor(255, 0, 255)
+    love.graphics.print("Hello world", 200, 100)
+
+    
+
 end
 
 -- Draw the game
 function Class:draw()
-	love.graphics.push()
-	
-    -- Apply virtual resolution before rendering anything
-    love.graphics.scale(self.virtualScaleFactor, self.virtualScaleFactor)
-	
-	-- Apply camera zoom
-	love.graphics.scale(self.zoom, self.zoom)
-	
-    -- Move to camera position
-    love.graphics.translate(
-		(self.virtualScreenHeight * 0.5 / self.zoom) * self.screenRatio - self.camera.x,
-		(self.virtualScreenHeight * 0.5 / self.zoom) - self.camera.y
-   	)
-	
-    -- Draw background
-	local screenExtent = vec2(self.virtualScreenHeight * self.screenRatio, self.virtualScreenHeight)
-	local cameraBounds = aabb(self.camera - screenExtent, self.camera + screenExtent)
+    self.station:draw()
 
-	-- Draw scene
-	love.graphics.setColor(255, 0, 0)
-	love.graphics.circle('fill', self.x, self.y, self.radius, 32)
-		
-	-- Reset camera transform before hud drawing
-	love.graphics.pop()
-
-	-- Draw HUD
-	love.graphics.setColor(255, 0, 0)
-	love.graphics.print("Hello world", 400, 300)
 end
 
 -----------------------------------------------------------------------------------------
---
+++ game/src/Station.lua
@@ -0,0 +1,103 @@
+-----------------------------------------------------------------------------------------
+--
+-- Station.lua
+--
+-- The station class.
+--
+-----------------------------------------------------------------------------------------
+module("Station", package.seeall)
+local Class = Station
+Class.__index = Class
+local sin = math.sin
+local cos = math.cos
+
+
+
+-- Create the station
+function Class.create(options)
+    -- Create object
+    self = {}
+    setmetatable(self, Class)
+
+    -- Set virtual viewport
+    self.virtualScreenHeight = gameConfig.camera.minVirtualHeight
+    self.virtualScaleFactor = love.graphics.getHeight() / self.virtualScreenHeight
+    self.screenRatio = love.graphics.getWidth() / love.graphics.getHeight()
+    self.camera = vec2(0, 0)
+    self.zoom = 1.0
+
+    -- Set font
+    love.graphics.setFont(love.graphics.newFont(40))
+
+    -- Create debug shape
+    self.x = 0
+    self.y = 0
+    self.radius = 100
+
+    self.axis1 = 0;
+    self.axis2 = 0;
+    self.axis4 = 0;
+    self.axis5 = 0;
+
+
+    return self
+end
+
+-- Destroy the station
+function Class:destroy()
+end
+
+-----------------------------------------------------------------------------------------
+-- Methods
+-----------------------------------------------------------------------------------------
+
+-- Update the station
+--
+-- Parameters:
+--  dt: The time in seconds since last frame
+function Class:update(dt)
+
+    -- Joystick
+    -- 1 X et 2 Y, left
+    -- 3  L2, R2
+    -- 4 Y et 5 X
+    self.axis1, self.axis2, self.axis3, self.axis4, self.axis5  = love.joystick.getAxes( 1 )
+
+
+end
+
+-- Draw the game
+function Class:draw()
+    love.graphics.push()
+    
+    -- Apply virtual resolution before rendering anything
+    love.graphics.scale(self.virtualScaleFactor, self.virtualScaleFactor)
+    
+    -- Apply camera zoom
+    love.graphics.scale(self.zoom, self.zoom)
+    
+    -- Move to camera position
+    love.graphics.translate(
+        (self.virtualScreenHeight * 0.5 / self.zoom) * self.screenRatio - self.camera.x,
+        (self.virtualScreenHeight * 0.5 / self.zoom) - self.camera.y
+    )
+    
+    -- Draw background
+    local screenExtent = vec2(self.virtualScreenHeight * self.screenRatio, self.virtualScreenHeight)
+    local cameraBounds = aabb(self.camera - screenExtent, self.camera + screenExtent)
+
+    -- Draw scene
+    love.graphics.setColor(0, 0, 255)
+    love.graphics.circle('line', self.x, self.y, self.radius, 32)
+    love.graphics.setColor(255, 0, 0)
+    love.graphics.line(0, 0, 100*self.axis1, 100*self.axis2)
+    love.graphics.setColor(0, 255, 0)
+    love.graphics.line(0, 0, 100*self.axis5, 100*self.axis4)
+        
+    -- Reset camera transform before hud drawing
+    love.graphics.pop()
+
+    -- Draw HUD
+    --love.graphics.setColor(255, 0, 255)
+    --love.graphics.print(self.axis1, 0, 0)
+end
