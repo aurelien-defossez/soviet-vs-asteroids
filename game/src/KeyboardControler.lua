7f86960f2f4a837da425c848983511e4d834dc08
Adrian Gaudebert <adrian@gaudebert.fr>
2013-03-23 10:46:12 +0100

    [Feature Keyboard] Added basic keyboard support, fallback to keyboard if no joystick is detected.

---
+++ game/src/KeyboardControler.lua
@@ -0,0 +1,76 @@
+-----------------------------------------------------------------------------------------
+--
+-- KeyboardControler.lua
+--
+-- The KeyboardControler class.
+--
+-----------------------------------------------------------------------------------------
+module("KeyboardControler", package.seeall)
+local Class = KeyboardControler
+Class.__index = Class
+local sin = math.sin
+local cos = math.cos
+
+-----------------------------------------------------------------------------------------
+-- Imports
+-----------------------------------------------------------------------------------------
+require("src.Config")
+require("src.Station")
+
+-- Create the KeyboardControler
+function Class.create(options)
+    -- Create object
+    self = {}
+    setmetatable(self, Class)
+
+    self.station = options.station
+
+    -- Set virtual viewport
+    self.virtualScreenHeight = gameConfig.camera.minVirtualHeight
+    self.virtualScaleFactor = love.graphics.getHeight() / self.virtualScreenHeight
+    self.screenRatio = love.graphics.getWidth() / love.graphics.getHeight()
+    self.camera = vec2(0, 0)
+    self.zoom = 1.0
+
+    return self
+end
+
+-- Destroy the KeyboardControler
+function Class:destroy()
+end
+
+-----------------------------------------------------------------------------------------
+-- Methods
+-----------------------------------------------------------------------------------------
+
+-- Update the KeyboardControler
+--
+-- Parameters:
+--  dt: The time in seconds since last frame
+function Class:update(dt)
+    deltaRad = math.pi / 36 -- 10°
+
+    -- Control the laser command
+    if (love.keyboard.isDown("left")) then
+        self.station:setLaserSatAngle(self.station.laserAngle - deltaRad)
+    end
+
+    if (love.keyboard.isDown("right")) then
+        self.station:setLaserSatAngle(self.station.laserAngle + deltaRad)
+    end
+
+    -- Control the missiles launcher
+    if (love.keyboard.isDown("a")) then
+        self.station:setMissileLauncherAngle(self.station.missileAngle - deltaRad)
+    end
+
+    if (love.keyboard.isDown("d")) then
+        self.station:setMissileLauncherAngle(self.station.missileAngle + deltaRad)
+    end
+end
+
+-- Draw the game
+function Class:draw()
+    love.graphics.push()
+    love.graphics.pop()
+end
