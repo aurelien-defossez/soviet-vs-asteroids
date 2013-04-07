ef9b73aeaa5681edcff6dbb8b83c7352cfbf76f7
Aurélien Defossez <defossez.aurelien@gmail.com>
2013-04-07 12:29:09 +0200

    Alex replaced by Mathieu in love config authors.

--- game/conf.lua
+++ game/conf.lua
@@ -1,6 +1,6 @@
 
 function love.conf(t)
     t.title = "Soviet VS Asteroids"
-    t.author = "Louis-Rémi Babé, Aurélien Defossez, Alex Frêne, Adrian Gaudebert, Frédéric Ostéréro, Maxime Viry"
+    t.author = "Louis-Rémi Babé, Aurélien Defossez, Adrian Gaudebert, Frédéric Ostéréro, Mathieu Perez, Maxime Viry"
     t.version = "0.8.0"
 end
